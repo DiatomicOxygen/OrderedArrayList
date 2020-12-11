36127e1b265c83fef51cf11a6db8547332007099
DiatomicOxygen <>
2020-12-11 17:54:01 -0500

    add(item) method works and helper method works

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -7,4 +7,24 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int x) {
     super(x);
   }
+
+  public int sorter(T value) {
+    for (int i = 0; i < size(); i ++) {
+      if (value.compareTo(get(i)) == -1 || value.compareTo(get(i)) == 0) {
+        return i;
+      }
+    }
+    return size() - 1;
+  }
+
+  public boolean add(T value) {
+    if (size() > 0) {
+      super.add(sorter(value), value);
+      return true;
+    } else {
+      super.add(value);
+      return true;
+    }
+  }
+
 }
