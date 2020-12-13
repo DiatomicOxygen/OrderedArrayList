0084a88a1364f2487b31e5d0f56bcf04b67ea700
DiatomicOxygen <>
2020-12-13 11:53:31 -0500

    sorter helper method fixed to account for nulls, set method fixed to only remove if value != null

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -9,31 +9,34 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   }
 
   public int sorter(T value) {
+    if (value == null) {
+      throw new IllegalArgumentException();
+    }
     for (int i = 0; i < size(); i ++) {
-      if (value.compareTo(get(i)) == -1 || value.compareTo(get(i)) == 0) {
+      if (value.compareTo(get(i)) <= 0) {
         return i;
       }
     }
     return size();
   }
 
-  public boolean add(T value) {
+  public boolean add(T value) throws IllegalArgumentException {
     if (size() > 0) {
       super.add(sorter(value), value);
       return true;
     } else {
-      super.add(value);
-      return true;
+      return super.add(value);
     }
   }
 
-  public void add(int index, T value) {
+  public void add(int index, T value) throws IllegalArgumentException {
     add(value);
   }
 
-  public T set(int index, T value) {
-    T removed = remove(index);
+  public T set(int index, T value) throws IllegalArgumentException{
+    T removed = get(index);
     add(value);
+    remove(removed);
     return removed;
   }
 }
